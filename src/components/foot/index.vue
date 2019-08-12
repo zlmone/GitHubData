00e3c0ef38445928c5df83be30ee9186dd52c70c
perye <11143526@qq.com>
2019-08-12 11:51:16 +0800

    update

---
+++ src/components/foot/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="footer-link">
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "foot"
+};
+</script>
+
+<style lang="scss">
+.footer-link {
+  position: relative;
+  font-size: 18px;
+  bottom: 0;
+  left: 0;
+  color: white;
+  text-align: center;
+  width: 100%;
+  z-index: 1001;
+  p {
+    a {
+      color: #e6e6fa;
+      cursor: pointer;
+    }
+  }
+}
+</style>
